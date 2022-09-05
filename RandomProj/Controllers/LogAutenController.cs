2e7fdb60c6a2a1771d204df40a94f28229f74450
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-05 13:47:11 +0300

    A

--- RandomProj/Controllers/LogAutenController.cs
+++ RandomProj/Controllers/LogAutenController.cs
@@ -24,8 +24,9 @@ namespace RandomProj.Controllers
             //return _context.Logins.Include(x => x.TipConcediu)
             //    .Select(x => new Concediu() { Id=x.Id, TipConcediu=x.TipConcediu })
             //    .Where(x => x.TipConcediu.Id==1).ToList();
+
             return _context.Logins.
-                Select(x => new Login() { Parola=x.Parola, Email=x.Email })
+                Select(x => new Login() { Parola=x.Parola, Email=x.Email, AngajatId = x.AngajatId })
                 .Where(x => x.Email==$"{email}").ToList();
         }
 

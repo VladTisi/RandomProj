4b7e2c67c43a15999db3c0ac7fe2ded8d6d8077a
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-05 10:01:53 +0300

    a

--- RandomProj/Controllers/WeatherForecastController.cs
+++ RandomProj/Controllers/WeatherForecastController.cs
@@ -27,7 +27,11 @@ namespace RandomProj.Controllers
         {
 
 
-            return _context.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu() { Id=x.Id, TipConcediu=x.TipConcediu }).Where(x => x.TipConcediu.Id==1).ToList();
+            return _context.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu()
+            { Id=x.Id, TipConcediu=x.TipConcediu }).Where(x => x.TipConcediu.Id==1).ToList();
         }
-    }
-}
+
+       
+    }   
+
+}     

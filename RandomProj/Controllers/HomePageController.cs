08a050454ad97f8f56a56189144a0cab647faf31
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-05 17:53:34 +0300

    a

--- RandomProj/Controllers/HomePageController.cs
+++ RandomProj/Controllers/HomePageController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RandomProj.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace RandomProj.Controllers
 {
@@ -15,15 +16,28 @@ namespace RandomProj.Controllers
             _logger = logger;
         }
 
-        [HttpPatch("UpdateZileCO")]
+        [HttpPatch("UpdateZile")]
 
         public void UpdateZileCO(int Id)
         {
             _context.Angajats.Where(x => x.Id == Id).FirstOrDefault();
-          
             int? ZileRamase = _context.Angajats.Where(x => x.Id == Id).Sum(x => EF.Functions.DateDiffMonth(x.DataAngajarii, DateTime.Now) * 2);
-            int? ZileConcediu = _context.Concedius.Where(x => x.AngajatId == Id).Sum(x => EF.Functions.DateDiffDay(x.DataInceput, x.DataSfarsit) - EF.Functions.DateDiffWeek(x.DataInceput, x.DataSfarsit) * 2);
-            if (ZileRamase <= ZileConcediu )
+            _context.Angajats.Where(x => x.Id == Id).FirstOrDefault().ZileConcediu = (int)ZileRamase;
+            _context.SaveChanges();
+
+
+        }
+
+
+        [HttpPatch("UpdateZileCORamase")]
+
+        public void UpdateZileCORamase(int Id)
+        {
+            _context.Angajats.Where(x => x.Id == Id).FirstOrDefault();
+
+            int? ZileTotale = _context.Angajats.Where(x => x.Id == Id).Sum(x => EF.Functions.DateDiffMonth(x.DataAngajarii, DateTime.Now) * 2);
+            int? ZileConcediu = _context.Concedius.Where(x => x.AngajatId == Id && x.StareConcediuId == 2).Sum(x => EF.Functions.DateDiffDay(x.DataInceput, x.DataSfarsit) - EF.Functions.DateDiffWeek(x.DataInceput, x.DataSfarsit) * 2);
+            if (ZileConcediu >= ZileTotale)
             {
                 _context.Angajats.Where(x => x.Id == Id).FirstOrDefault().ZileConcediuRamase = 0;
                 _context.SaveChanges();
@@ -33,16 +47,43 @@ namespace RandomProj.Controllers
 
             else
             {
-                _context.Angajats.Where(x => x.Id == Id).FirstOrDefault().ZileConcediuRamase = ((int)ZileRamase - (int)ZileConcediu);
+                _context.Angajats.Where(x => x.Id == Id).FirstOrDefault().ZileConcediuRamase = ((int)ZileTotale - (int)ZileConcediu);
                 _context.SaveChanges();
                 return;
             }
-                                       
-                   
-           
+        }
+
+            [HttpPatch("UpdateTelfPoza")]
+
+            public void UpdateDateDeUtilizator(string? numarTelefon, string? email, string? poza, int Id)
+
+            {
+                var myObj = _context.Angajats.Where(x => x.Id == Id).FirstOrDefault();
+                if (myObj == null)
+                {
+                    return;
+                }
+                              
+                myObj.NumarTelefon = String.IsNullOrEmpty(numarTelefon) ? myObj.NumarTelefon : numarTelefon;
+                myObj.Poza = String.IsNullOrEmpty(poza) ? myObj.Poza : poza;
+
+                _context.SaveChanges();
+            }
+
+
+            [HttpPatch("UpdateEmail")]
+
+            public void UpdateEmail(string email, int Id)
+            {
+
+            _context.Logins.Where(x => x.AngajatId == Id).FirstOrDefault().Email = email;
+            _context.SaveChanges();
+
+             }
+
+
         }
 
 
 
     }
-}

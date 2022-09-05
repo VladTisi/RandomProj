007470621c6b4ce4903c9154f1fc008503fcee60
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-05 10:32:27 +0300

    a

---
+++ RandomProj/Controllers/MeniuModificareDateAngajat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomProj.Models;
+
+namespace RandomProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeniuModificareDateAngajat : Controller
+    {
+        
+      
+
+            PrisonBreakContext _context;
+
+            public MeniuModificareDateAngajat(PrisonBreakContext context)
+            {
+                _context = context;
+            }
+         
+     }
+    
+}

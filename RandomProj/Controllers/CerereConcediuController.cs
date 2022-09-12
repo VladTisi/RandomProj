d2e63de59f5f776be8758e6beb988426da732b6b
Vladut Mielu <vladut.mielu@totalsoft.ro>
2022-09-12 10:18:04 +0300

    s

--- RandomProj/Controllers/CerereConcediuController.cs
+++ RandomProj/Controllers/CerereConcediuController.cs
@@ -39,7 +39,7 @@ namespace RandomProj.Controllers
         [HttpGet("GetZileNeplatite")]
         public int GetZileNeplatite(int angajatId)
         {
-            int ZileTotale = 365;
+            int ZileTotale = 30;
             int ZileConcediu = (int)_context.Concedius.Where(x => ((DateTime)x.DataInceput).Year == DateTime.Now.Year && x.TipConcediuId == 3 && x.StareConcediuId == 2).Sum(x => EF.Functions.DateDiffDay(x.DataInceput, x.DataSfarsit)+1 - EF.Functions.DateDiffWeek(x.DataInceput, x.DataSfarsit) * 2);
             if (ZileTotale - ZileConcediu == null)
                 return 0;

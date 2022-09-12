6a50722e1d8316d197d6cb07893fca44e60910e2
CosminTotal <cosmin.gostin@totalsoft.ro>
2022-09-12 09:29:17 +0300

    s

--- RandomProj/Controllers/GestionareConcediiController.cs
+++ RandomProj/Controllers/GestionareConcediiController.cs
@@ -53,6 +53,21 @@ namespace RandomProj.Controllers
             return lista;
         }
 
+        [HttpGet("GetConcediuAcceptate")]
+
+        public List<Concediuedt> GetConcediuAcceptate(int angajatId)
+        {
+            var lista = new List<Concediuedt>();
+            var user = _context.Angajats.FirstOrDefault(x => x.Id == angajatId);
+            return _context.Concedius
+                   .Include(x => x.Angajat)
+                   .Include(x => x.Angajat.Functie)
+                   .Include(x => x.StareConcediu)
+                   .Where(x => x.StareConcediuId == 2 && x.StareConcediu.Id == x.StareConcediuId)
+                   .Select(x => new Concediuedt { Id = x.Id, Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Functie = x.Angajat.Functie.Nume, Status = x.StareConcediu.Nume, DataInceput = x.DataInceput, DataSfarsit = x.DataSfarsit }).ToList();
+            return lista;
+        }
+
         [HttpGet("GetConcedii")]
         public List<Concediuedt> GetConcedii(int angajatId)
         {
